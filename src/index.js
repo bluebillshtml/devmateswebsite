fc153c026b4d452351bfc9778a5f235081b5a250
bluebillshtml <carplevi@outlook.com>
2025-10-24 20:01:52 -0400

    FirstRealCommit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LandingPage from './components/LandingPage';
+import './components/LandingPage.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
